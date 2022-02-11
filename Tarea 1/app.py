8eadee1920598a350a8945a109b8a7f5f88d28cf
Lino Garcia <lgarciavallejo@paypal.com>
2022-02-11 11:58:18 -0600

    Tarea 1

---
+++ Tarea 1/app.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import filedialog as fd
+import re as re
+
+filetypes = [("Archivos de Entrada", "*.txt")]
+def AbrirArchivo():
+    nombreArchivo = fd.askopenfilename(filetypes=filetypes, title="Seleccione un archivo .txt")
+    if nombreArchivo == '':
+        return
+    tf = open(nombreArchivo, 'r')
+    contenido = tf.read()
+    tf.close()
+    patron = '[A-Z]{1}';
+    print('Total de Letras:', len(re.findall(patron, contenido)));
+    patron = '\d'
+    print('Total de Dígitos:', len(re.findall(patron, contenido)));
+    patron = '[^a-zA-Z\d\n\s|]';
+    print('Total de Símbolos:', len(re.findall(patron, contenido)));
+
+
+errmsg = 'Error!'
+tk.Button(text='Click para seleccionar archivo de entrada', command=AbrirArchivo).pack(fill=tk.X)
+tk.mainloop()
+
