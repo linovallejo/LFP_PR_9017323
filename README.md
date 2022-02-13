8dd4104a5fbfebe25cfebfd483b859f5a7c0f5af
Lino Garcia Vallejo <2274031850101@ingenieria.usac.edu.gt>
2022-02-12 21:04:48 -0600

    Grafica de Pie - Ejemplo, Cambios a README

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Lenguajes Formales y de Programación - Práctica
 
 Lino Antonio García Vallejo.  
 Carnet 9017323
+
+Configuracion de ambiente de desarrollo:
+
+Instalar Python 3.8
+Instalar pip3 
+
+Instalar paquetes requeridos
+* pip3 install tkinter
+* pip3 install consolemenu
+* pip3 install -U matplotlib
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from consolemenu import *
 from consolemenu.items import *
 import tkinter as tk
 from tkinter import filedialog as fd
+import matplotlib.pyplot as plt
 
 
 def AbrirArchivoData():
@@ -27,6 +28,19 @@ def AbrirArchivoInstrucciones():
 def Analizar():
     print('Analizar')
 
+def GraficaPie():
+    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    sizes = [15, 30, 45, 10]
+    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    plt.show()
+
 menu = ConsoleMenu("Generador de Reportes de Ventas", exit_option_text="Salir")
 
 menu_item = MenuItem("Menu Item")
@@ -34,10 +48,12 @@ menu_item = MenuItem("Menu Item")
 function_item_cargar_datos = FunctionItem("Cargar Data", AbrirArchivoData)
 function_item_cargar_instrucciones = FunctionItem("Cargar Instrucciones", AbrirArchivoInstrucciones)
 function_item_analizar = FunctionItem("Analizar", Analizar)
+function_item_grafica = FunctionItem("Grafica", GraficaPie)
 
 menu.append_item(function_item_cargar_datos)
 menu.append_item(function_item_cargar_instrucciones)
 menu.append_item(function_item_analizar)
+menu.append_item(function_item_grafica)
 
 menu.show()
 
